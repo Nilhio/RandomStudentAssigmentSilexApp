f9691faac41f24dcb9b84166cc4e68ce3dd6aca2
Edvinas <edvinas@ito.lt>
2016-12-19 14:48:16 +0200

    Fixed routing issues

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/config/config.php';
+require_once __DIR__.'/config/form.php';
+require_once __DIR__.'/config/routes.php';
+
+
+$app->run();
--- web/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../config/config.php';
-require_once __DIR__.'/../config/form.php';
-require_once __DIR__.'/../config/routes.php';
-
-
-$app->run();
